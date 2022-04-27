e94a76015c57ffdf4c7ae00c10ecd6a9f68e558b
l7rf1i82 <msdo24ws@gmail.com>
2022-04-27 14:18:32 +0600

    upd

---
+++ src/lib/iblock/new/exec_delete_property.php
@@ -0,0 +1,97 @@
+<?php Bitrix\Main\Loader::includeModule("iblock");
+
+
+use Bitrix\Iblock\IblockTable;
+use Bitrix\Iblock\PropertyTable;
+use Bitrix\Iblock\ElementPropertyTable;
+use Bitrix\Iblock\PropertyEnumerationTable;
+use Bitrix\Iblock\PropertyFeatureTable;
+
+
+$data = Customer\Data::DATA;
+
+
+$container = $data['container'];     // list
+
+
+$itqex1 = IblockTable::query()
+    ->setFilter(['CODE' => $container['CODE']])
+    ->setSelect(['ID']);
+$itqex1result1 = $itqex1->exec()->fetch();
+$iblockId = $itqex1result1['ID'];                                       // get iblock id
+
+
+$idListProperty  = [];
+$ptqex1pq = PropertyTable::query()
+    ->setFilter(['IBLOCK_ID' => $iblockId])
+    ->setSelect(['CODE', 'ID'])
+    ->exec();
+while ($ptqex1prow = $ptqex1pq->fetch())
+{
+    if(!in_array($ptqex1prow['CODE'], $container['PROPERTY']))
+    {
+        continue;
+    }
+
+    array_push($idListProperty, $ptqex1prow['ID']);              // get property id
+}
+
+$idListPropertyEnum = [];
+$petglex1peq   = PropertyEnumerationTable::query()
+    ->setFilter(['PROPERTY_ID' => $idListProperty])
+    ->setSelect(['ID'])
+    ->exec();
+while($petglex1perow = $petglex1peq->fetch())
+{
+    array_push($idListPropertyEnum, $petglex1perow['ID']);       // get property enum id
+}
+
+
+$idListPropertyFeature = [];
+$ptqex1pfq = PropertyFeatureTable::query()
+    ->setFilter(['PROPERTY_ID' => $idListProperty])
+    ->setSelect(['ID'])
+    ->exec();
+while($ptqex1pfrow = $ptqex1pfq->fetch())
+{
+    array_push($idListPropertyFeature, $ptqex1pfrow['ID']);      // get property feature id
+}
+
+
+$idListElementProperty = [];
+$eptglex1prq = ElementPropertyTable::query()
+    ->setFilter(['IBLOCK_PROPERTY_ID' => $idListProperty])
+    ->setSelect(['ID'])
+    ->exec();
+while($eptglex1prrow= $eptglex1prq->fetch())
+{
+    array_push($idListElementProperty, $eptglex1prrow['ID']);    // get element property id
+}
+
+
+
+
+foreach ($idListElementProperty as $id)
+{
+    $itu1result = ElementPropertyTable::getByPrimary($id)->fetchObject()->delete();
+}
+
+foreach ($idListPropertyEnum as $id)
+{
+    $petdex1qu = PropertyEnumerationTable::getByPrimary([
+        'ID' => $id
+    ])->fetchObject();
+    $petdex1result = $petdex1qu->delete();
+}
+
+foreach ($idListPropertyFeature as $id)
+{
+    $ptd1 = PropertyFeatureTable::getByPrimary($id)->fetchObject()->delete();
+}
+
+foreach ($idListProperty as $id)
+{
+    $ptd1 = PropertyTable::getByPrimary($id)->fetchObject()->delete();
+}
+
+
---
+++ src/usr/iblock/delete_property.php
@@ -0,0 +1,20 @@
+<?php // data for init property
+
+
+namespace Customer;
+
+
+class Data
+{
+    const DATA = [
+        'container' => [
+            'CODE' => 'supplier-steel',
+            'PROPERTY' => [
+                "ORDER_COST_MINIMUM",
+                "ORDER_MUNUFACTURE_TIME",
+                "PAYMENT_TYPE",
+                "PAYMENT_BANK",
+            ]
+        ]
+    ];
+}
